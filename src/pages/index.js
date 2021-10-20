1cad8646927aebd40305ff341851de4b91647a4f
marekreid <marekreid@gmail.com>
2021-10-20 05:26:36 +0200

    added landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ const IndexPage = ({data}) => {
   const menuItems = [
     "Introduction",
     "Gallery",
-    "OurFamily",
+    "Our Family",
     "Parties & Events",
     "Partner Venues",
     "Enquiries",
