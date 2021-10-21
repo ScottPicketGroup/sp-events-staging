348e9cc00afc6baa7f3f8f094b25af9d636073fc
marekreid <marekreid@gmail.com>
2021-10-21 07:21:01 +0200

    working window undefined in useWindowDimensions hook

--- src/components/Common/Hooks/useWindowDimensions.js
+++ src/components/Common/Hooks/useWindowDimensions.js
@@ -12,6 +12,7 @@ export default function useWindowSize() {
   }
 
   React.useEffect(() => {
+    setWindowSize({ width: window.innerWidth, height: window.innerHeight });
     window.addEventListener("resize", changeWindowSize);
 
     return () => {
