b49dc693ee59d55eef491e97c819b44ebf7c8074
marekreid <marekreid@gmail.com>
2021-10-22 14:44:56 +0200

    added learn more expand

--- src/components/Common/Rich-Text-Renderers/Intro.js
+++ src/components/Common/Rich-Text-Renderers/Intro.js
@@ -2,9 +2,18 @@ import React from 'react'
 import { BLOCKS} from "@contentful/rich-text-types"
 import { renderRichText } from "gatsby-source-contentful/rich-text"
 import { Heading1, Heading2, Heading3, BC1 } from "../../StyledComponents/typography.css"
+import styled from 'styled-components';
+const Renderer = ({ node, isExpanded }) => {
+const [wrapperHeight, setWrapperHeight] = React.useState(0)
+const [openWrapperHeight, setOpenWrapperHeight] = React.useState(0)
+const paragraphRef = React.useRef(null)
+const wrapperRef= React.useRef(null)
+React.useEffect(() => {
+    setWrapperHeight(paragraphRef.current.clientHeight)
+    setOpenWrapperHeight(wrapperRef.current.clientHeight)
+}, [])
 
-const Renderer = ({ node }) => {
-
+console.log(paragraphRef.current)
     const options = {
         // renderMark: {
         //     [MARKS.BOLD]: text => <Bold>{text}</Bold>,
@@ -13,16 +22,25 @@ const Renderer = ({ node }) => {
             [BLOCKS.HEADING_1]: (node, children) => <Heading1>{children}</Heading1>,
             [BLOCKS.HEADING_2]: (node, children) => <Heading2>{children}</Heading2>,
             [BLOCKS.HEADING_3]: (node, children) => <Heading3>{children}</Heading3>,
-            [BLOCKS.PARAGRAPH]: (node, children) => <BC1 marginBottom="sm">{children}</BC1>
+            [BLOCKS.PARAGRAPH]: (node, children, i) => <IntroWrapper><BC1 marginBottom="sm" ref={paragraphRef} wrapperHeight='100' isExpanded={isExpanded}>{children}</BC1> </IntroWrapper>
         },
     }
 
     return (
-        <>
+        <IntroWrapper wrapperHeight={wrapperHeight} isExpanded={isExpanded} ref={wrapperRef} openWrapperHeight={openWrapperHeight}>
 
-            {renderRichText(node, options)}
-        </>
+            { renderRichText(node, options)}
+        </IntroWrapper>
     )
 }
 
 export default Renderer
+
+export const IntroWrapper = styled.div`
+  max-height: ${props => props.isExpanded ? `${props.openWrapperHeight }px` : `${ props.wrapperHeight + 20}px`};
+  overflow: hidden;
+  margin-bottom: 1rem;
+  transition: max-height 0.5s ease;
+`;
+
+// paragraphRef && 
--- src/components/Pages/Venues/Intro.js
+++ src/components/Pages/Venues/Intro.js
@@ -4,11 +4,17 @@ import { SectionContainer } from "../../StyledComponents/containers.css"
 import {  Heading1, Heading2 } from "../../StyledComponents/typography.css"
 
 const Intro = ({introduction}) => {
+  const [firstParagraphHeight, setFirstParagraphHeight] =  React.useState(0)
+
+  const [isExpanded, setIsExpanded] = React.useState(false)
+  console.log(isExpanded)
   return (
     <SectionContainer marginBottom="xl" >
       <Heading1 marginBottom="sm">Lorem ipsum dolor sit amet, consetetur</Heading1>
-      <Renderer node={introduction} />
-      <Heading2> Learn More</Heading2>
+      <Renderer node={introduction} isExpanded={isExpanded}/>
+      <Heading2
+      onClick={() => setIsExpanded(!isExpanded)}
+      > Learn More</Heading2>
     </SectionContainer>
   )
 }
