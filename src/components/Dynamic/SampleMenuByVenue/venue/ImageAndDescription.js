1d66b5ec1076fbeed31b7cdd14c97fa6ec50a18e
Marek <marekreid@gmail.com>
2022-11-30 15:21:33 +1100

    added link compatibility to subevents grid

--- src/components/Dynamic/SampleMenuByVenue/venue/ImageAndDescription.js
+++ src/components/Dynamic/SampleMenuByVenue/venue/ImageAndDescription.js
@@ -20,7 +20,7 @@ const ImageAndDescription = ({ venue }) => {
       <PartnerVenuesLandingWrapper>
         <GatsbyImage image={getImage(venue.venueImage)} alt="Chancery Lane" />
         <TextContainer>
-          <Heading2 marginBottom="xs">{venue.venueName}</Heading2>
+          <Heading2 marginBottom="xs">{venue.headingRight}</Heading2>
           <Heading3 marginBottom="sm">{venue.subheading}</Heading3>
          <SPGRestaurantListRenderer node={venue.description} />
           {/* <Heading3>
