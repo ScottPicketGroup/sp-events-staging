be1188842e515ce2acece8c4591e3a30c7572034
Marek <marekreid@gmail.com>
2022-12-15 07:48:01 +1100

    updated site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,10 +4,10 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Scott Pickett Events`,
+    description: ``,
+    author: `@marek.reid`,
+    siteUrl: `https://www.spevents.com.au`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
