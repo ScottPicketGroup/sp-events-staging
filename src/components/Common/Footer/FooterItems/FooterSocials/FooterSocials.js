57c79aac7ebcedcdc6f3d5dd20cbd7ffdc9a2ee0
Marek <marekreid@gmail.com>
2023-01-17 04:44:20 +0100

    swapped social links back around

--- src/components/Common/Footer/FooterItems/FooterSocials/FooterSocials.js
+++ src/components/Common/Footer/FooterItems/FooterSocials/FooterSocials.js
@@ -11,19 +11,19 @@ const FooterSocials = () => {
     <FooterLinksColumn>
       <FooterItem marginBottom="md">SOCIALS</FooterItem>
       <FooterExternalLink
+        href="https://www.instagram.com/scottpickettevents/"
+        target="_blank"
+        rel="no referer"
+      >
+        Facebook
+      </FooterExternalLink>
+      <FooterExternalLink
         marginBottom="sm"
         href=" https://www.facebook.com/profile.php?id=100072255153533"
         target="_blank"
         rel="no referer"
       >
         Instagram
-      </FooterExternalLink>
-      <FooterExternalLink
-        href="https://www.instagram.com/scottpickettevents/"
-        target="_blank"
-        rel="no referer"
-      >
-        Facebook
       </FooterExternalLink>
     </FooterLinksColumn>
   )
