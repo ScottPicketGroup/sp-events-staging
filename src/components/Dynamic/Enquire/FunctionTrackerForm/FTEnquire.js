406a7a0a63ab0ac81e5ec85f7938d399368afdf5
Marek <marekreid@gmail.com>
2023-11-14 09:21:19 +0100

    updated packages

--- src/components/Dynamic/Enquire/FunctionTrackerForm/FTEnquire.js
+++ src/components/Dynamic/Enquire/FunctionTrackerForm/FTEnquire.js
@@ -1,64 +1,62 @@
-// import React, { useRef } from "react"
+import React, { useRef } from "react"
 
-// import {
-//   Heading1,
-//   Heading2,
-//   IntroLink,
-// } from "../../StyledComponents/typography.css"
-// import Renderer from "../../Common/Rich-Text-Renderers/IntroRenderer"
-// import ArrowIcon from "../../../images/ArrowIcon"
-// import EnquireForm from "./EnquireForm"
-// import {
-//   SectionContainer,
-//   SectionWrapper,
-//   executeScroll,
-// } from "../../StyledComponents/containers.css"
-// import FunctionTrackerForm from "./FunctionTrackerForm/FunctionTrackerForm"
-// const FTEnquire = ({ data, page }) => {
-//   const {
-//     enquireHeading,
-//     greyBackground,
-//     enquireDescription,
-//     linkLabel,
-//     linkUrl,
-//   } = data
+import {
+  Heading1,
+  Heading2,
+  IntroLink,
+} from "../../../StyledComponents/typography.css"
+import {
+  SectionContainer,
+  SectionWrapper,
+} from "../../../StyledComponents/containers.css"
+import Renderer from "../../../Common/Rich-Text-Renderers/IntroRenderer"
+import ArrowIcon from "../../../../images/ArrowIcon"
+import FunctionTrackerForm from "./FunctionTrackerForm"
+const FTEnquire = ({ data, page }) => {
+  const {
+    enquireHeading,
+    greyBackground,
+    enquireDescription,
+    linkLabel,
+    linkUrl,
+  } = data
 
-//   const heading = useRef()
-//   React.useEffect(() => {
-//     const isBrowser = typeof window !== "undefined"
-//     if (!isBrowser) {
-//       return
-//     }
+  const heading = useRef()
+  React.useEffect(() => {
+    const isBrowser = typeof window !== "undefined"
+    if (!isBrowser) {
+      return
+    }
 
-//     return console.log(
-//       "form info:",
-//       window && window.ftenquireinit(1180, "spevents")
-//     )
-//   }, [])
-//   return (
-//     <SectionContainer
-//       paddingTop={page === "enquiries" ? "lg" : "xl"}
-//       isGreyBackground={greyBackground}
-//       width="100vw"
-//       paddingBottom="lg"
-//     >
-//       <SectionWrapper width="73.5%">
-//         <Heading1 marginBottom="sm" ref={heading}>
-//           {enquireHeading}
-//         </Heading1>
-//         <Renderer node={enquireDescription} />
-//         {linkLabel && (
-//           <IntroLink to={`/${linkUrl}`}>
-//             {" "}
-//             <Heading2>
-//               {linkLabel} <ArrowIcon />
-//             </Heading2>
-//           </IntroLink>
-//         )}
-//         <FunctionTrackerForm />
-//       </SectionWrapper>
-//     </SectionContainer>
-//   )
-// }
+    return console.log(
+      "form info:",
+      window && window.ftenquireinit(1180, "spevents")
+    )
+  }, [])
+  return (
+    <SectionContainer
+      paddingTop={page === "enquiries" ? "lg" : "xl"}
+      isGreyBackground={greyBackground}
+      width="100vw"
+      paddingBottom="lg"
+    >
+      <SectionWrapper width="73.5%">
+        <Heading1 marginBottom="sm" ref={heading}>
+          {enquireHeading}
+        </Heading1>
+        <Renderer node={enquireDescription} />
+        {linkLabel && (
+          <IntroLink to={`/${linkUrl}`}>
+            {" "}
+            <Heading2>
+              {linkLabel} <ArrowIcon />
+            </Heading2>
+          </IntroLink>
+        )}
+        <FunctionTrackerForm />
+      </SectionWrapper>
+    </SectionContainer>
+  )
+}
 
-// export default FTEnquire
+export default FTEnquire
