d7ef6f1a638fce5f77782763b6c182b679bd17c4
Lmarcho-NeoSolax <your_email@example.com>
2024-08-15 09:41:09 +0530

    Null Check for menuItems

--- src/components/Common/Footer/FooterItems/FooterVenueLinks/FooterVenueLinks.js
+++ src/components/Common/Footer/FooterItems/FooterVenueLinks/FooterVenueLinks.js
@@ -1,33 +1,38 @@
-import { Link } from "gatsby"
-import React from "react"
-import useMenudata from "../../../../Menu/MenuSlideOut/menuEventTypesQuery"
-import { FooterExternalLink, FooterLink, FooterLinksColumn } from "../../Footer.css"
+import React from "react";
+import useMenudata from "../../../../Menu/MenuSlideOut/menuEventTypesQuery";
+import { FooterExternalLink, FooterLink, FooterLinksColumn } from "../../Footer.css";
 
 const FooterVenueLinks = () => {
-
   const links = [
     { name: "Chancery Lane", to: "http://www.chancerylane.com.au" },
-    { name: "Estelle", to: " http://www.theestelle.com.au" },
-    { name: "Longrain", to: " http://www.longrainmelbourne.com" },
-    { name: "Longsong", to: " http://www.longsong.com.au" },
-    { name: "Le Shoppe", to: " http://www.leshoppe.com.au"},
-    { name: "Matilda", to: " http://www.matilda159.com" },
-  ]
-  const menuItems = useMenudata()
+    { name: "Estelle", to: "http://www.theestelle.com.au" },
+    { name: "Longrain", to: "http://www.longrainmelbourne.com" },
+    { name: "Longsong", to: "http://www.longsong.com.au" },
+    { name: "Le Shoppe", to: "http://www.leshoppe.com.au" },
+    { name: "Matilda", to: "http://www.matilda159.com" },
+  ];
+
+  const menuItems = useMenudata();
+
   return (
-    <FooterLinksColumn>
-      <FooterExternalLink marginBottom="md">EXPERIENCES</FooterExternalLink>
-      { menuItems[1].eventTypes.map((item, index) => (
-       
-        <FooterLink marginBottom="sm" to={`/events/${item.pageName.toLowerCase().replace(" ", "-")}`} key={index}>     {item.pageName}</FooterLink>
-       
-      ))}
-       
-       
-      
+      <FooterLinksColumn>
+        <FooterExternalLink marginBottom="md">EXPERIENCES</FooterExternalLink>
+        {menuItems[1] && menuItems[1].eventTypes && menuItems[1].eventTypes.map((item, index) => (
+            <FooterLink
+                marginBottom="sm"
+                to={`/events/${item.pageName.toLowerCase().replace(" ", "-")}`}
+                key={index}
+            >
+              {item.pageName}
+            </FooterLink>
+        ))}
+        {links.map((link, index) => (
+            <FooterExternalLink marginBottom="sm" href={link.to} key={index}>
+              {link.name}
+            </FooterExternalLink>
+        ))}
+      </FooterLinksColumn>
+  );
+};
 
-    </FooterLinksColumn>
-  )
-}
-
-export default FooterVenueLinks
+export default FooterVenueLinks;
